c980d7153c976b746b17a363d8f53b70fe3964ca
Romserg <serg.romankov@gmail.com>
2019-03-13 19:02:39 +0200

    Create state in parent component

--- src/index.js
+++ src/index.js
@@ -5,16 +5,33 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square"
+              onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i){
+    const squares = [...this.state.squares];
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]}
+                   onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
@@ -48,7 +65,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -62,6 +79,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Game/>,
   document.getElementById('root')
 );
